0362ff02bd03fb6bd7411b801b1bd5ec916f2cd7
Avila <avilarosee332002@gmail.com>
2023-07-21 15:19:01 +0530

    Abcde

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         student s1 = new student();
-        s1.name = "bala";
+        s1.name = "aarthi";
         s1.dept = "ECE";
         System.out.println(s1.name);
         System.out.println(s1.dept);
